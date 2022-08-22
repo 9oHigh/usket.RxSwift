b39240ebe92a45975d6c34de1102c6ac15bb2d96
9oHigh <wkdska957@naver.com>
2022-08-22 22:47:08 +0900

    Add: RxSwift Project

---
+++ usket.RxSwift/usket.RxSwift/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  usket.RxSwift
+//
+//  Created by 이경후 on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ usket.RxSwift/usket.RxSwiftUITests/usket_RxSwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  usket_RxSwiftUITestsLaunchTests.swift
+//  usket.RxSwiftUITests
+//
+//  Created by 이경후 on 2022/08/22.
+//
+
+import XCTest
+
+class usket_RxSwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
